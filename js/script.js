ae57651181151662cc2b5d97f348d95c51522e78
Balpreet Singh <singhbalpreet007@gmail.com>
2023-07-06 19:14:31 -0400

    Edit TODO.txt Implement animate functionality in style.css Add rock, paper and scissors images Implement basic structure in index.html Refactor script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
+const gameDiv = document.querySelector(".game");
+const you = document.querySelector(".you")
+const computer = document.querySelector(".computer")
 const choices = ["rock", "scissors", "paper"];
+let info = document.querySelector(".info");
+const buttons = document.querySelectorAll("button");
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getPlayerChoice() {
-    return prompt("Rock, Paper or Scissors?").toLowerCase();
+    return new Promise((resolve) => {        
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                resolve(button.className);
+            });
+        });
+    });
 }
 
-function playRound(playerSelection, computerSelection) {
+function getResult(playerSelection, computerSelection) {
+    let result;
     if (getWinner(playerSelection, computerSelection) === playerSelection) {
-        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        result = `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
     } else if (getWinner(playerSelection, computerSelection) === computerSelection) {
-        return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+        result = `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
     } else {
-        return "Oh it's a Tie!";
+        result = "Oh it's a Tie!";
     }
+    return result;
 }
 
 function getWinner(first, second) {
     let choices = ["rock", "scissors", "paper"];
     let indexFirst = choices.indexOf(first);
     let indexSecond = choices.indexOf(second);
-
-    // if (first === second) {
-    //     return null;
-    // }
 
     switch (indexFirst - indexSecond) {
         case -1:
@@ -36,9 +45,6 @@ function getWinner(first, second) {
         default:
             return second;
     }
-    // return ((indexFirst - indexSecond === -1) || (indexFirst - indexSecond == 2)) 
-    //     ? first
-    //     : second;
 }
 
 function capitalize(word) {
@@ -46,46 +52,54 @@ function capitalize(word) {
     return word.replace(word[0], firstLetter);
 }
 
-function stuff() {
-    return 1 < 2 ? "yup" : "nope"
-}
+async function game() {
+    console.log("Starting...");
+    let playerScoreOnScoreboard = document.querySelector(".player-score");
+    let computerScoreOnScoreboard = document.querySelector(".computer-score");
+    let info = document.querySelector(".info");
+    console.log(info);
 
-function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
         let computerChoice = getComputerChoice();
         console.log(`Computer is ${computerChoice}`);
-        let playerChoice = getPlayerChoice();
-        let result = playRound(playerChoice, computerChoice);
-        console.log(result);
+        let playerChoice = await getPlayerChoice();
+        console.log(playerChoice);
+
+        let result = getResult(playerChoice, computerChoice);
+        info.innerHTML = result;
+        console.log(info.textContent);
         
         if (result.includes("Win")) {
             playerScore += 1;
+            playerScoreOnScoreboard.textContent = playerScore;
         } 
         if (result.includes("Lose")) {
             computerScore += 1;
+            computerScoreOnScoreboard.textContent = computerScore;
         }
 
         console.log(playerScore, computerScore)
     }
 
     let score = playerScore === computerScore
-        ? "It's a tie!"
+        ? "It's a Tie!"
         : playerScore > computerScore
             ? `You Win ${playerScore}-${computerScore}!`
             : `You Lose ${playerScore}-${computerScore}!`
 
     console.log(score)
+
+    const finalResult = document.createElement("div");
+    finalResult.classList.add("new");
+    you.classList.add("new-other");
+    computer.classList.add("new-other");
+    gameDiv.insertBefore(finalResult, computer);
+    finalResult.textContent = score;
+    finalResult.style.cssText = "align-self: center; font-size: 40px; color: green"
+    console.log(finalResult.innerHTML);
 }
 
 game();
-
-// console.log(playRound("rock", "scissors"));
-// console.log(playRound("scissors", "rock"));
-// console.log(capitalize("jfklsj"));
-
-
-
-// console.log(playRound(getPlayerChoice(), computer));
